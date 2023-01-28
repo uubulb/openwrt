9efce9f2c3e34b44f8d285c2ce20bd8933a0ccf4
Applexad <35923940+applexad@users.noreply.github.com>
2023-01-28 20:48:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,5 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
